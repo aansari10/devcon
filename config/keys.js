37d0d59d966b146b58e396be81afc31497e76cb0
root <root@localhost.localdomain>
2019-08-17 16:45:31 +0530

    Separated keys file for dev and prod

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://devcon:devcon@devcon-epjwz.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
