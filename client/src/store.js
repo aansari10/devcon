f3ef40c13c244c873e1393f0a5034f133520502a
root <root@localhost.localdomain>
2019-08-21 20:34:47 +0530

    Fixed issue of redux tools

--- client/src/store.js
+++ client/src/store.js
@@ -10,7 +10,10 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    process.env.NODE_ENV === "production"
+      ? null
+      : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
