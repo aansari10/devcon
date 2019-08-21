94e72ff5970fefced7178a1a975b4efc8ec55f42
root <root@localhost.localdomain>
2019-08-21 18:51:38 +0530

    Preparing for deploy

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,6 +24,16 @@ mongoose
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Static assets if in production server
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Passport middleware
 app.use(passport.initialize());
