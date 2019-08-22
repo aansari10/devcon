e3247f23c0e544075ec4fafd924ed8bc2956f13d
root <root@localhost.localdomain>
2019-08-22 18:53:55 +0530

    Changed footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,11 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-dark text-white mt-5 p-3 text-center ">
-      Copyright &copy; {new Date().getFullYear()} DevCon
+      Copyright &copy; {new Date().getFullYear()} DevCon{" "}
+      <span style={{ float: "right" }}>
+        Dev By:
+        <strong>Aamir Ansari</strong>
+      </span>
     </footer>
   );
 }
